4579ee9bbe69c86110e7a0251d669c980896a1c2
Wink Saville <wink@saville.com>
2022-12-13 12:39:02 -0800

    feat: Test scoping

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,58 @@
 use exper_drop::{HasTwoDrops, HasDrop};
 
 fn main() {
-    let x = HasTwoDrops {
-        name: "HasTwoDrops".to_owned(),
+    println!("main:+");
+    let outer_h2d = HasTwoDrops {
+        name: "outer_h2d".to_owned(),
         one: HasDrop {
-            name: "one".to_owned(),
+            name: "outer_h2d.one".to_owned(),
         },
         two: HasDrop {
-            name: "two".to_owned(),
+            name: "outer_h2d.two".to_owned(),
         },
     };
-    println!("x: {:?}", x);
-    println!("Running!");
+    println!("outer_h2d: {outer_h2d:?}");
+
+    println!();
+    for i in 1..=3 {
+        println!("TOL 1.{i}");
+
+        #[allow(unused_labels)]
+        'inner: {
+            let inner_h2d= HasTwoDrops {
+                name: "inner_h2d".to_owned(),
+                one: HasDrop {
+                    name: "inner_h2d.one".to_owned(),
+                },
+                two: HasDrop {
+                    name: "inner_h2d.two".to_owned(),
+                },
+            };
+            println!("inner_h2d: {inner_h2d:?}");
+            // inner_h2d dropped here
+        }
+
+        println!("BOL 1.{i}");
+    }
+
+    println!();
+    for i in 1..=3 {
+        println!("TOL 2.{i}");
+
+        let inner_h2d= HasTwoDrops {
+            name: "inner_h2d".to_owned(),
+            one: HasDrop {
+                name: "inner_h2d.one".to_owned(),
+            },
+            two: HasDrop {
+                name: "inner_h2d.two".to_owned(),
+            },
+        };
+        println!("inner_h2d: {inner_h2d:?}");
+
+        println!("BOL 2.{i}");
+        // inner_h2d dropped here
+    }
+
+    println!("main:-");
 }
